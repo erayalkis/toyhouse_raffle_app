c5bc1a44b1aa4bbaaf389f825f3bc2cb5ceea556
erayalkis <erayalkis@hotmail.com>
2022-06-11 23:20:51 +0300

    Write code for parsing ID from URL

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--https://toyhouse-rails-api.herokuapp.com/raffle/10868863.-yui- -->
   <div class="app">
     <form id="load-user-form" @submit.prevent="loadCharacter">
       <input name="load-user-input" placeholder="Enter raffle character URL..." />
@@ -10,7 +11,13 @@
 
   const loadCharacter = (e) => {
     const characterUrl = e.target.elements[0].value;
-    console.log(characterUrl);
+    const characterId = parseCharacterUrl(characterUrl);
+    console.log(characterId);
+  }
+
+  const parseCharacterUrl = (url) => {
+    const characterId = url.split("/")[3];
+    return characterId
   }
 
 </script>
