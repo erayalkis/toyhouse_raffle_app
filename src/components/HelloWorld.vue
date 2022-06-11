a4cc7c39df07cc1fc7b66c51bb20f54a6faef4e7
erayalkis <erayalkis@hotmail.com>
2022-06-12 01:42:04 +0300

    Create models for options

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,18 +3,34 @@
   <div class="app">
     <form id="load-user-form" @submit.prevent="loadCharacter">
       <input name="load-user-input" placeholder="Enter raffle character URL..." v-model="urlInput" />
+      <button type="submit">Load Characters</button>
     </form>
+
+    <ul>
+      <li>
+        <label for="shouldComment">User should comment?</label>
+        <input type="checkbox" id="shouldComment" v-model="shouldComment" />
+      </li>
+      <li>
+        <label for="shouldSubscribe">User should subscribe?</label>
+        <input type="checkbox" id="shouldSubscribe" v-model="shouldSub" />
+      </li>
+    </ul>
 
     <p v-if="messages.error">{{messages.error}}</p>
     <p v-if="messages.loading">{{messages.loading}}</p>
+
   </div>
 </template>
 
 <script setup>
   import { messages } from '@/state/messages';
+  import { participants } from '@/state/participants';
   import { ref } from 'vue';
   
   const urlInput = ref('');
+  const shouldComment = ref(false);
+  const shouldSub = ref(false);
 
   const loadCharacter = async () => {
     messages.clearError();
@@ -28,8 +44,8 @@
     const characterId = parseCharacterUrl(characterUrl);
     console.log(characterId);
 
-    const attendees = await fetchTickets(characterId);
-    console.log(attendees)
+    const users = await fetchTickets(characterId);
+    participants.setParticipants(users);
     urlInput.value = '';
   }
 
@@ -39,8 +55,9 @@
   }
 
   const fetchTickets = async (id) => {
-    const users = await fetch(`https://toyhouse-rails-api.herokuapp.com/raffle/${id}`);
-    return users.json();
+    const users = await fetch(`https://toyhouse-rails-api.herokuapp.com/raffle/${id}?`);
+    console.log(users.json);
+    return await users.json();
   }
 
 </script>
