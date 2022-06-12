d40762c01dad14a3690e1d593c5e13fc3d97db7c
erayalkis <erayalkis@hotmail.com>
2022-06-12 03:06:41 +0300

    Set up vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import CharacterForm from './components/CharacterForm.vue';
+import ParticipantsList from './components/ParticipantsList.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: CharacterForm },
+  { path: '/participants', component: ParticipantsList },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
