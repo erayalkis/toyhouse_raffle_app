f9e26c52eb6b94c0797c467c3928cfb1e3baf04f
erayalkis <erayalkis@hotmail.com>
2022-06-12 03:07:10 +0300

    Create Participants.vue

---
+++ src/components/ParticipantsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--https://toyhouse-rails-api.herokuapp.com/raffle/10868863.-yui- -->
+  <div class="participants-list">
+    <template v-for="(details, username) in participants.list" :key="username">
+      <div class="participant">
+        <p>Name: {{username}}</p>
+        <p>Tickets: {{details.ticket_count}}</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+  import { participants } from '@/state/participants';
+</script>
+
+<style scoped>
+  .participants-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
