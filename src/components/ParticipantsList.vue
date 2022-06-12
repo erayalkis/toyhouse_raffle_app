2a47bd911ed7f665db55dae12e7ce485e6c769d3
erayalkis <erayalkis@hotmail.com>
2022-06-12 17:00:01 +0300

    Implement debouncing

--- src/components/ParticipantsList.vue
+++ src/components/ParticipantsList.vue
@@ -6,11 +6,11 @@
     </div>
   </template>
   <template v-else>
-    <input type="text" placeholder="Search user by name..." v-model="query" />
+    <input type="text" placeholder="Search user by name..." @input="createDebounce" />
     <div class="participants-wrapper">
       <div class="participants-list">
-        <template v-for="(details, username) in filteredUsers" :key="username">
-          <ParticipantCard :details="details" :username="username" />
+        <template v-for="user in filteredUsers" :key="user[0]">
+          <ParticipantCard :details="user[1]" :username="user[0]" />
         </template>
       </div>
     </div>
@@ -22,23 +22,36 @@
   import { computed, ref } from 'vue';
   import ParticipantCard from './ParticipantCard.vue';
 
-
   const query = ref('');
+  let timer;
 
   const filteredUsers = computed(() => {
-    if(query.value.length == 0) return participants.list;
-    const newObj = {};
+    if(query.value.length == 0) return Object.entries(participants.list);
+    // const newObj = {};
 
-    let keys = Object.keys(participants.list);
+    // let keys = Object.keys(participants.list);
 
-    keys.forEach((key) => {
-      if(key.toLowerCase().includes(query.value.toLowerCase())) {
-        newObj[key] = participants.list[key];
-      }
-    })
+    // keys.forEach((key) => {
+    //   if(key.toLowerCase().includes(query.value.toLowerCase())) {
+    //     newObj[key] = participants.list[key];
+    //   }
+    // })
 
-    return newObj;
+    let filtered = Object.entries(participants.list).filter(
+      ([key]) => key.toLowerCase().includes(query.value.toLowerCase()) 
+    )
+
+    console.log(filtered);
+    return filtered;
   })
+
+  function createDebounce(event) {
+    if(timer) timer = clearTimeout(timer);
+
+    timer = setTimeout(function() {
+      query.value = event.target.value;
+    }, 500)
+  }
 </script>
 
 <style scoped>
