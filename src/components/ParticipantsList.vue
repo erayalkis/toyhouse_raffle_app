37a7c21b6fb9478cbc0183438cba5288d47f28a7
erayalkis <erayalkis@hotmail.com>
2022-06-12 03:27:02 +0300

    Style participants list

--- src/components/ParticipantsList.vue
+++ src/components/ParticipantsList.vue
@@ -1,12 +1,15 @@
 <template>
   <!--https://toyhouse-rails-api.herokuapp.com/raffle/10868863.-yui- -->
-  <div class="participants-list">
-    <template v-for="(details, username) in participants.list" :key="username">
-      <div class="participant">
-        <p>Name: {{username}}</p>
-        <p>Tickets: {{details.ticket_count}}</p>
-      </div>
-    </template>
+  <div class="participants-wrapper">
+    <div class="participants-list">
+      <template v-for="(details, username) in participants.list" :key="username">
+        <div class="participant">
+          <img class="participant-image" :src="details.image" />
+          <p>Name: {{username}}</p>
+          <p>Tickets: {{details.ticket_count}}</p>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -15,8 +18,29 @@
 </script>
 
 <style scoped>
+
+  .participants-wrapper {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+  }
   .participants-list {
     display: flex;
     flex-wrap: wrap;
+    width: 60%;
+    flex-shrink: 5;
+  }
+
+  .participant {
+    margin: 30px;
+    text-align: center;
+    border: 1px solid lightblue;
+  }
+
+  .participant-image {
+    border-bottom: 1px solid lightblue;
+    width: 200px;
+    height: 200px;
   }
 </style>
