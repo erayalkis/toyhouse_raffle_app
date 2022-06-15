47203ce149ed526387205fd040ba4272b0ea55f9
erayalkis <erayalkis@hotmail.com>
2022-06-15 21:46:11 +0300

    Style app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-    <p>
+    <nav class="navbar">
       <!-- use the router-link component for navigation. -->
       <!-- specify the link by passing the `to` prop. -->
       <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-      <router-link class="router-link" to="/">Go to Home</router-link>
-      <router-link class="router-link" to="/participants">Go to Participants</router-link>
-    </p>
+      <div class="nav-links">
+        <router-link class="router-link" to="/">Home</router-link>
+        <router-link class="router-link" to="/participants">Participants</router-link>
+      </div>
+      <div class="app-status">
+        <h3>Offline</h3>
+      </div>
+    </nav>
     <!-- route outlet -->
     <!-- component matched by the route will render here -->
     <router-view></router-view>
@@ -30,7 +35,28 @@
   height: 100vh;
 }
 
-.router-link {
-  margin: 5px;
+.app-status {
+  margin-right: 15px;
 }
+
+.navbar {
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+  width: 100vw;
+  height: 4em;
+  border-bottom: 1px solid black;
+  box-sizing: border-box;
+}
+
+.router-link {
+  margin: 0 30px 0 15px;
+  background-color: gray;
+  display: inline-block;
+  padding: 1.4em;
+  width: 8em;
+}
+
 </style>
