a3e2ee54fc6fae9186a0521330d0eefc698313aa
erayalkis <erayalkis@hotmail.com>
2022-06-16 04:47:25 +0300

    Change label text

--- src/components/CharacterForm.vue
+++ src/components/CharacterForm.vue
@@ -21,7 +21,7 @@
             <input type="checkbox" id="shouldComment" v-model="shouldComment" />
           </div>
               <div class="shouldCommentInput">
-                <label for="commentCount">How many extra tickets?</label>
+                <label for="commentCount">Comment ticket count</label>
                 <br />
                 <input :disabled="!shouldComment" type="number" min="0" v-model="commentCount" />
               </div>
@@ -32,7 +32,7 @@
             <input type="checkbox" id="shouldSubscribe" v-model="shouldSub" />
           </div>
               <div class="shouldSubInput">
-                <label for="subCount">How many extra tickets?</label>
+                <label for="subCount">Subscription ticket count</label>
                 <br />
                 <input :disabled="!shouldSub" type="number" min="0" v-model="subCount" />
               </div>
