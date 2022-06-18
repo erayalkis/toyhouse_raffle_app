cf23ed4100be2a80bd422b8085870bdcad253881
erayalkis <erayalkis@hotmail.com>
2022-06-18 22:52:18 +0300

    Remove dist from gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/toyhouse_raffle_app/'
+  publicPath: '/toyhouse_raffle_app'
 })
