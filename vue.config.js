dc6e63f44ff3b290425bf9f99f6371e40bf9a1b3
erayalkis <erayalkis@hotmail.com>
2022-06-18 22:46:11 +0300

    Add slashes to URL

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/toyhouse_raffle_app/'
 })
