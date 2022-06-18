4d326ecf9ccf2c3eadad4d90d0cda41b704e473a
erayalkis <erayalkis@hotmail.com>
2022-06-19 00:14:24 +0300

    Fix footer link

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
     <footer>
       <small><em>
         This app is not associated with or a part of Toyhou.se. It's only a fan project. Check the repository out
-          <a class="footer-link" href="https:/github.com/erayalkis/toyhouse_raffle_App" target="#">here</a>
+          <a class="footer-link" href="https://github.com/erayalkis/toyhouse_raffle_App" target="#">here</a>
       </em></small>
     </footer>
 </template>
