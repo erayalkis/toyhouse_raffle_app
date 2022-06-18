c8058c4d1502cbb66a31bd34893e60792e5ad3e4
erayalkis <erayalkis@hotmail.com>
2022-06-18 21:42:48 +0300

    Fix swapped sub and comment counts being used

--- src/components/CharacterForm.vue
+++ src/components/CharacterForm.vue
@@ -144,12 +144,12 @@
 
     if(shouldComment.value) { 
       base += 'must_comment=true&';
-      base += `comment_ticket_count=${subCount.value}&`
+      base += `comment_ticket_count=${commentCount.value}&`
     }
 
     if(shouldSub.value) {
       base += 'must_subscribe=true&';
-      base += `subscribe_ticket_count=${commentCount.value}&`;
+      base += `subscribe_ticket_count=${subCount.value}&`;
     }
 
     return base;
