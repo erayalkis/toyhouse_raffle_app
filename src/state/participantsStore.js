949c44f5023b321df7df4ed111b16f6abfc28635
Eray Alkış <lolpo12345@outlook.com>
2023-07-15 01:56:03 +0300

    Add code to pickWinners

--- src/state/participantsStore.js
+++ src/state/participantsStore.js
@@ -26,7 +26,34 @@ export const useParticipantsStore = defineStore("participants", () => {
     user.ticket_count -= 1;
   };
 
-  const pickWinners = () => {};
+  const pickWinners = (winnersCount) => {
+    const usersDupedByTicketCount = [];
+
+    list.value.forEach((user) => {
+      let i = user.ticket_count;
+      for (i; i > 0; i--) {
+        usersDupedByTicketCount.push(user.profile);
+      }
+    });
+
+    // Set of usernames
+    let seenUsers = new Set();
+    let winners = [];
+
+    for (winnersCount; winnersCount > 0; winnersCount--) {
+      const idx = getRandomIndex(usersDupedByTicketCount.value.length);
+      const selectedUser = usersDupedByTicketCount[idx];
+
+      const username = selectedUser.profile.name;
+      let inSeen = seenUsers.has(username);
+      if (!inSeen) {
+        seenUsers.add(username);
+        winners.push(selectedUser);
+      }
+    }
+
+    return winners;
+  };
 
   const getRandomIndex = (length) => Math.floor(Math.random() * length);
   const rerollWinner = () => {};
