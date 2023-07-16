d5f53252a7ea16f76fe28a8a18dd8c37dff3cfb8
Eray Alkış <lolpo12345@outlook.com>
2023-07-16 19:54:52 +0300

    Add optionsStore

---
+++ src/state/optionsStore.js
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useOptionsStore = defineStore("options", () => {
+  const defaultOptions = {
+    character_id: "",
+    must_comment: false,
+    comment_points: 1,
+    must_subscribe: false,
+    subscribe_points: 1,
+  };
+
+  const characters = ref(null);
+
+  const resetCharacter = (character) => {
+    let characterId = character.id;
+
+    let characterIdx = characters.value.findIndex(
+      (chr) => chr.id === characterId
+    );
+    let newOpts = defaultOptions;
+    newOpts.character_id = characterId;
+
+    characters.value[characterIdx] = newOpts;
+  };
+
+  const updateCharacter = (character) => {
+    let characterId = character.id;
+
+    let characterIdx = characters.value.findIndex(
+      (chr) => chr.id === characterId
+    );
+
+    characters.value[characterIdx] = character;
+  };
+
+  const removeCharacter = (character) => {
+    let characterId = character.id;
+
+    characters.value = characters.value.filter((chr) => chr.id !== characterId);
+  };
+
+  return { characters, resetCharacter, updateCharacter, removeCharacter };
+});
