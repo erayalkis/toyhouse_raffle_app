e924c9b37a2c7fc4d7b6c37a2032fa08253c07df
Eray Alkış <lolpo12345@outlook.com>
2023-07-17 01:03:03 +0300

    Set up pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue/dist/vue.esm-bundler";
 import { router } from "@/router/router.js";
 import App from "./App.vue";
 import "./index.css";
+import { createPinia } from "pinia";
 
+const pinia = createPinia();
 const app = createApp(App);
+
 app.use(router);
+app.use(pinia);
 app.mount("#app");
