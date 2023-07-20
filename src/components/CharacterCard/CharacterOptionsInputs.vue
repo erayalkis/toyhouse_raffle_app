e5073b88f5395884496af56056b8f385868ce8c5
Eray Alkış <lolpo12345@outlook.com>
2023-07-20 19:30:50 +0300

    Use v-model and :disabled attributes in CharacterOptionsInputs.vue

--- src/components/CharacterCard/CharacterOptionsInputs.vue
+++ src/components/CharacterCard/CharacterOptionsInputs.vue
@@ -2,6 +2,31 @@
   <div class="md:flex md:justify-between">
     <div class="mb-5">
       <div class="flex items-center gap-1.5">
+        <h1 class="text-lg">Should favorite?</h1>
+        <input
+          type="checkbox"
+          class="m-0 p-0 mt-1"
+          v-model="mainOpt.must_favorite"
+          checked
+          disabled
+        />
+      </div>
+      <div class="gap-1 mt-2">
+        <div class="flex gap-1">
+          <StarIcon class="w-5" />
+          <h1>Favorite points:</h1>
+        </div>
+
+        <input
+          type="number"
+          class="rounded-sm disabled:bg-neutral-200"
+          v-model="mainOpt.favorite_points"
+          :disabled="!mainOpt.must_favorite"
+        />
+      </div>
+    </div>
+    <div class="mb-5">
+      <div class="flex items-center gap-1.5">
         <h1 class="text-lg">Should subscribe?</h1>
         <input
           type="checkbox"
@@ -15,7 +40,12 @@
           <h1>Subscription points:</h1>
         </div>
 
-        <input type="number" v-model="mainOpt.subscribe_points" />
+        <input
+          type="number"
+          class="rounded-sm disabled:bg-neutral-200"
+          v-model="mainOpt.subscribe_points"
+          :disabled="!mainOpt.must_subscribe"
+        />
       </div>
     </div>
     <div class="mb-5">
@@ -32,7 +62,12 @@
           <CommentIcon class="w-5" />
           <h1>Comment points:</h1>
         </div>
-        <input type="number" v-model="mainOpt.comment_points" />
+        <input
+          type="number"
+          class="rounded-sm disabled:bg-neutral-200"
+          v-model="mainOpt.comment_points"
+          :disabled="!mainOpt.must_comment"
+        />
       </div>
     </div>
   </div>
@@ -43,6 +78,7 @@ import { storeToRefs } from "pinia";
 import { useOptionsStore } from "@/state/optionsStore.js";
 import SubscribeIcon from "@/assets/components/SubscribeIcon.vue";
 import CommentIcon from "@/assets/components/CommentIcon.vue";
+import StarIcon from "@/assets/components/StarIcon.vue";
 
 let optsStore = useOptionsStore();
 let { opts } = storeToRefs(optsStore);
