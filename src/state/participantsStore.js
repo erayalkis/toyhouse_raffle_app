48d80dcfe840248f55b47663687da4a75e4b34b4
Eray Alkış <lolpo12345@outlook.com>
2023-07-20 19:51:00 +0300

    Switch to an object-based system for picking winners

--- src/state/participantsStore.js
+++ src/state/participantsStore.js
@@ -3,74 +3,65 @@ import { computed, ref } from "vue";
 
 export const useParticipantsStore = defineStore("participants", () => {
   const list = ref({});
-  const winners = ref([]);
-  const winnersDuped = ref([]);
-  const loaded = computed(() => list.value.length === 0);
+  const winners = ref({});
+  const usersByTicketCount = ref([]);
+  const loaded = computed(() => Object.keys(list.value).length === 0);
 
-  const setParticipants = (arr) => (list.value = arr);
+  const setParticipants = (obj) => (list.value = obj);
 
   const deleteParticipants = () => {
     list.value = [];
     winners.value = [];
   };
 
-  const remove = (u) =>
-    (list.value = list.value.filter((user) => user.id != u.id));
+  const remove = (user) => delete list.value[user];
 
-  const increment = (u) => {
-    const user = list.value.find((user) => (user.id = u.id));
-    user.ticket_count += 1;
+  const increment = (user) => {
+    list.value[user].ticket_count += 1;
   };
 
-  const decrement = (u) => {
-    const user = list.value.find((user) => (user.id = u.id));
-    user.ticket_count -= 1;
+  const decrement = (user) => {
+    list.value[user].ticket_count -= 1;
   };
 
   const pickWinners = (winnersCount) => {
-    setDupedWinners();
+    setDupedUsers();
 
     // Set of usernames
     let seenUsers = new Set();
-    let winners = [];
+    let winners = {};
 
     for (winnersCount; winnersCount > 0; winnersCount--) {
-      const idx = getRandomIndex(winnersDuped.value.length);
-      const selectedUser = winnersDuped.value[idx];
+      const idx = getRandomIndex(usersByTicketCount.value.length);
+      const selectedUser = usersByTicketCount.value[idx];
 
       const username = selectedUser.profile.name;
       let inSeen = seenUsers.has(username);
       if (!inSeen) {
         seenUsers.add(username);
-        winners.push(selectedUser);
+        winners[username] = selectedUser;
       }
     }
 
     return winners;
   };
 
-  const setDupedWinners = () => {
-    winnersDuped.value = [];
+  const setDupedUsers = () => {
+    usersByTicketCount.value = [];
 
     const usersDupedByTicketCount = [];
 
-    list.value.forEach((user) => {
-      let i = user.ticket_count;
-      for (i; i > 0; i--) {
-        usersDupedByTicketCount.push(user.profile);
-      }
+    Object.keys(list.value).forEach((username) => {
+      let user = list.value[username];
+      usersDupedByTicketCount.push(user);
     });
 
-    winnersDuped.value = usersDupedByTicketCount;
+    usersByTicketCount.value = usersDupedByTicketCount;
   };
 
   const getRandomIndex = (length) => Math.floor(Math.random() * length);
   const rerollWinner = (username) => {
-    const targetIdx = winners.value.findIndex(
-      (user) => user.profile.name === username
-    );
-
-    const dupedWinnersWithoutTarget = winnersDuped.value.filter(
+    const dupedWinnersWithoutTarget = usersByTicketCount.value.filter(
       (user) => user.profile.name != username
     );
 
@@ -78,7 +69,7 @@ export const useParticipantsStore = defineStore("participants", () => {
       dupedWinnersWithoutTarget.value.length
     );
 
-    winners.value[targetIdx] = dupedWinnersWithoutTarget[newWinnerIndex];
+    winners.value[username] = dupedWinnersWithoutTarget[newWinnerIndex];
   };
 
   return {
