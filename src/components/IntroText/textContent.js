dd560a5fa8653d2121b63a7565788ec54c62a88b
Eray Alkış <lolpo12345@outlook.com>
2023-09-05 02:09:17 +0300

    Update text content

--- src/components/IntroText/textContent.js
+++ src/components/IntroText/textContent.js
@@ -2,5 +2,4 @@ export const textContent = [
   "This is a free tool for Toyhou.se that let's you pick raffle winners efficiently!",
   "Simply select the rules, and load your character!",
   "You can then adjust your participants in the tab on the top left!",
-  "Psst! This site recently got a desktop app, check it out at https://github.com/erayalkis/toyhouse_api/releases/latest",
 ];
